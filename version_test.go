9fe150092f7218ebe82378ad32913af23af73c5f
knqyf263 <knqyf263@gmail.com>
2017-07-16 18:17:24 +0900

    Add rpmvercmp

--- version_test.go
+++ version_test.go
@@ -57,11 +57,125 @@ func TestNewVersion(t *testing.T) {
 
 	for _, tc := range cases {
 		actual := NewVersion(tc.version)
-
 		if !reflect.DeepEqual(actual, tc.expected) {
 			t.Fatalf(
 				"version: %s\nexpected: %v\nactual: %v",
 				tc.version, tc.expected, actual)
+		}
+	}
+}
+
+func TestEqual(t *testing.T) {
+	for _, tc := range cases {
+		expected := (tc.expected == 0)
+
+		// compare 'version'
+		a := Version{0, tc.v1, ""}
+		b := Version{0, tc.v2, ""}
+		if actual := a.Equal(b); actual != expected {
+			t.Errorf("[Version] v1: %s\nv2: %s\nexpected: %v\nactual: %v",
+				tc.v1, tc.v2, expected, actual)
+		}
+
+		// compare 'release'
+		a = Version{0, "", tc.v1}
+		b = Version{0, "", tc.v2}
+		if actual := a.Equal(b); actual != expected {
+			t.Errorf("[Release] v1: %s\nv2: %s\nexpected: %v\nactual: %v",
+				tc.v1, tc.v2, tc.expected, actual)
+		}
+	}
+}
+
+func TestGreaterThan(t *testing.T) {
+	for _, tc := range cases {
+		expected := tc.expected > 0
+
+		// compare 'version'
+		a := Version{0, tc.v1, ""}
+		b := Version{0, tc.v2, ""}
+		if actual := a.GreaterThan(b); actual != expected {
+			t.Errorf("[Version] v1: %s\nv2: %s\nexpected: %v\nactual: %v",
+				tc.v1, tc.v2, expected, actual)
+		}
+
+		// compare 'release'
+		a = Version{0, "", tc.v1}
+		b = Version{0, "", tc.v2}
+		if actual := a.GreaterThan(b); actual != expected {
+			t.Errorf("[Release] v1: %s\nv2: %s\nexpected: %v\nactual: %v",
+				tc.v1, tc.v2, tc.expected, actual)
+		}
+	}
+}
+
+func TestLessThan(t *testing.T) {
+	for _, tc := range cases {
+		expected := tc.expected < 0
+
+		// compare 'version'
+		a := Version{0, tc.v1, ""}
+		b := Version{0, tc.v2, ""}
+		if actual := a.LessThan(b); actual != expected {
+			t.Errorf("[Version] v1: %s\nv2: %s\nexpected: %v\nactual: %v",
+				tc.v1, tc.v2, expected, actual)
+		}
+
+		// compare 'release'
+		a = Version{0, "", tc.v1}
+		b = Version{0, "", tc.v2}
+		if actual := a.LessThan(b); actual != expected {
+			t.Errorf("[Release] v1: %s\nv2: %s\nexpected: %v\nactual: %v",
+				tc.v1, tc.v2, tc.expected, actual)
+		}
+	}
+}
+
+func TestCompare(t *testing.T) {
+	for _, tc := range cases {
+		// compare 'version'
+		a := Version{0, tc.v1, ""}
+		b := Version{0, tc.v2, ""}
+		if actual := a.Compare(b); actual != tc.expected {
+			t.Errorf("[Version] v1: %s\nv2: %s\nexpected: %v\nactual: %v",
+				tc.v1, tc.v2, tc.expected, actual)
+		}
+
+		// compare 'release'
+		a = Version{0, "", tc.v1}
+		b = Version{0, "", tc.v2}
+		if actual := a.Compare(b); actual != tc.expected {
+			t.Errorf("[Release] v1: %s\nv2: %s\nexpected: %v\nactual: %v",
+				tc.v1, tc.v2, tc.expected, actual)
+		}
+	}
+}
+
+func TestString(t *testing.T) {
+	cases := []struct {
+		v        Version
+		expected string
+	}{
+		{Version{2, "7.4.052", "1.el6"}, "2:7.4.052-1.el6"},
+		{Version{2, "7.4.052", "1"}, "2:7.4.052-1"},
+		{Version{0, "7.4.052", "1"}, "7.4.052-1"},
+	}
+	for _, tc := range cases {
+		actual := tc.v.String()
+		if actual != tc.expected {
+			t.Fatalf("v: %v\n\nexpected: %v\nactual: %v",
+				tc.v, tc.expected, actual)
+		}
+	}
+}
+
+func TestRpmVerCmp(t *testing.T) {
+	// Import cases from version_testcase.go
+	for _, tc := range cases {
+		actual := rpmvercmp(tc.v1, tc.v2)
+		if tc.expected != actual {
+			t.Fatalf("v1: %s\nv2: %s\nexpected: %v\nactual: %v",
+				tc.v1, tc.v2, tc.expected, actual)
 		}
 	}
 }
